9bbb171069c011b8d65e0f1b256172315178dfa2
bshardi <bart.hardison@gmail.com>
2014-02-03 21:59:10 -0500

    styling meetingPage and adding tools menu

--- client/views/meetings/meeting_page.js
+++ client/views/meetings/meeting_page.js
@@ -2,6 +2,9 @@ Template.meetingPage.helpers({
 	openMeetingText: function() {
 		return this.open ? "Close meeting" : "Open Meeting";
 	},
+	meetingStatus: function() {
+		return this.active ? "Status: Open" : "Status: Closed"
+	},
 	attendees: function() {
 		return Attendees.find();
 	},
@@ -14,10 +17,12 @@ Template.meetingPage.events({
 	"click .remove-attendee": function(e){
 		e.preventDefault();
 		if (confirm("Do you want to remove this attendee?")){
-			Meteor.call("removeAttendee", this.meetingId, this._id);
+			Meteor.call("removeAttendee", this.meetingId, this._id, function(error, id){
+				if (error) throwError(error.reason);
+			});
 		}
 	},
-	"click .pick-winner": function(e){
+	"click .pick-winner-btn": function(e){
 		e.preventDefault();
 	    Meteor.call("selectWinner", this._id, function(error, id) {
 	      if (error) 
